b71e5772ed9d1f8d02b13b5215906da3667bbaff
杨庆鑫 <alan@s-MacBook-puro.local>
2018-04-08 14:38:00 +0800

    add readme

---
+++ Readme.md
@@ -0,0 +1,13 @@
+## 抖音视频爬虫
+
+## Requirements
+`
+Python3.6 + pip
+`
+
+## 启动方式
+`
+cd to/project
+pip install -r requirements.pip
+scrapy crawl douyin
+`
--- deepcrawl/extensions/redisfilter.py
+++ deepcrawl/extensions/redisfilter.py
@@ -5,14 +5,14 @@ from scrapy.utils.request import request_fingerprint
 
 class RedisURLFilter(BaseDupeFilter, redis.StrictRedis):
 
-  def __init__(self, *args, **kwargs):
-    self.key = kwargs.pop('key', 'Crawl::redisURLDupeKey')
-    redis.StrictRedis.__init__(self, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.key = kwargs.pop('key', 'Crawl::redisURLDupeKey')
+        redis.StrictRedis.__init__(self, **kwargs)
 
-  @classmethod
-  def from_settings(cls, settings):
-    return cls(host = settings.get('REDIS_HOST'), port = settings.getint('REDIS_PORT'), password = settings.get('REDIS_PASSWORD'))
+    @classmethod
+    def from_settings(cls, settings):
+        return cls(host = settings.get('REDIS_HOST'), port = settings.getint('REDIS_PORT'), password = settings.get('REDIS_PASSWORD'))
 
-  def request_seen(self, request):
-    fp = request_fingerprint(request)
-    return self.sadd(self.key, fp)
+    def request_seen(self, request):
+        fp = request_fingerprint(request)
+        return self.sadd(self.key, fp) == 0
